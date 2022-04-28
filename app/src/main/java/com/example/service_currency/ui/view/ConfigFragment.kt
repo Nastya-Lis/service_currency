6b45fd08f97609cbb7edf4a9051129a634b58eb7
Nastya-Lis <bankay.kotopes@mail.ru>
2022-04-28 22:17:43 +0300

    start in compose

--- app/src/main/java/com/example/service_currency/ui/view/ConfigFragment.kt
+++ app/src/main/java/com/example/service_currency/ui/view/ConfigFragment.kt
@@ -1,84 +1,148 @@
 package com.example.service_currency.ui.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.Toolbar
+import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.ItemTouchHelper.DOWN
-import androidx.recyclerview.widget.ItemTouchHelper.UP
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.service_currency.R
-import com.example.service_currency.adapter.RecyclerConfigCurrencyAdapter
 import com.example.service_currency.data.db.CurrencyEntity
 import com.example.service_currency.ui.viewmodel.ConfigViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.util.*
 
 class ConfigFragment : Fragment() {
 
     private val viewModel: ConfigViewModel by viewModels()
-    var listOfShit: List<CurrencyEntity> = listOf()
-
-
-    private val itemTouchHelper by lazy {
-        val itemTouchCallback = object: ItemTouchHelper.SimpleCallback(UP or DOWN,0) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                val recyclerviewAdapter = recyclerView.adapter as RecyclerConfigCurrencyAdapter
-                val fromPosition = viewHolder.absoluteAdapterPosition
-                val toPosition = target.absoluteAdapterPosition
-               // Collections.swap(listOfShit, fromPosition,toPosition )
-                recyclerviewAdapter.moveItem(fromPosition, toPosition)
-                recyclerviewAdapter.notifyItemMoved(fromPosition,toPosition)
-                return false
-            }
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-
-            }
-        }
-        ItemTouchHelper(itemTouchCallback)
-    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_config, container, false)
+        viewModel.getCurrency("2022-04-29", requireContext())
+        return ComposeView(requireContext()).apply {
+            setContent { ConfigScreen() }
+        }
+    }
 
-        val recycler: RecyclerView = view.findViewById(R.id.config_currency_recycler)
-        recycler.layoutManager = LinearLayoutManager(requireContext())
-        itemTouchHelper.attachToRecyclerView(recycler)
-        val recyclerAdapter = RecyclerConfigCurrencyAdapter(requireContext())
 
-        viewModel.mutableListConfigCurrency.observe(requireActivity()){
-            if(it != null){
-                //listOfShit = it
-                recyclerAdapter.differ.submitList(it as MutableList)
-                recycler.adapter = recyclerAdapter
-                recycler.adapter?.notifyDataSetChanged()
+    @Composable
+    fun ConfigScreen() {
+        return Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "Настройка валют") },
+                    navigationIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_baseline_navigate_before_48),
+                            contentDescription = "back"
+                        )
+                    },
+                    actions = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_baseline_check_48),
+                                contentDescription = "save"
+                            )
+                        }
+                    }
+                )
+            })
+        {
+            LazyColumn {
+                items(items = viewModel.listStateCurrency) { item ->
+                    Row {
+                        Column {
+                            LaunchedEffect(item) {
+                                Log.d("Hui", item.curId)
+                            }
+                            Text(item.curId, fontWeight = FontWeight.Bold)
+                            Text("${item.scale}  ${item.name}")
+                        }
+                        Switch(checked = item.checked, onCheckedChange = { item.checked = it })
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_baseline_view_headline_24),
+                            contentDescription = ""
+                        )
+                    }
+                }
             }
         }
+    }
+
+    /*   private val viewModel: ConfigViewModel by viewModels()
+       var listOfShit: List<CurrencyEntity> = listOf()
 
 
-        view.findViewById<Toolbar>(R.id.topAppBar).setOnMenuItemClickListener {
-            when (it.itemId) {
+       private val itemTouchHelper by lazy {
+           val itemTouchCallback = object: ItemTouchHelper.SimpleCallback(UP or DOWN,0) {
+               override fun onMove(
+                   recyclerView: RecyclerView,
+                   viewHolder: RecyclerView.ViewHolder,
+                   target: RecyclerView.ViewHolder
+               ): Boolean {
+                   val recyclerviewAdapter = recyclerView.adapter as RecyclerConfigCurrencyAdapter
+                   val fromPosition = viewHolder.absoluteAdapterPosition
+                   val toPosition = target.absoluteAdapterPosition
+                  // Collections.swap(listOfShit, fromPosition,toPosition )
+                   recyclerviewAdapter.moveItem(fromPosition, toPosition)
+                   recyclerviewAdapter.notifyItemMoved(fromPosition,toPosition)
+                   return false
+               }
 
-                R.id.accept_setting -> {
-                    viewModel.updateCurrenciesInDb(requireContext(),
-                        viewModel.mutableListConfigCurrency.value as MutableList/*listOfShit*/)
+               override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+               }
+           }
+           ItemTouchHelper(itemTouchCallback)
+       }
+
+       override fun onCreateView(
+           inflater: LayoutInflater, container: ViewGroup?,
+           savedInstanceState: Bundle?
+       ): View? {
+           // Inflate the layout for this fragment
+           val view = inflater.inflate(R.layout.fragment_config, container, false)
+
+           val recycler: RecyclerView = view.findViewById(R.id.config_currency_recycler)
+           recycler.layoutManager = LinearLayoutManager(requireContext())
+           itemTouchHelper.attachToRecyclerView(recycler)
+           val recyclerAdapter = RecyclerConfigCurrencyAdapter(requireContext())
+
+           viewModel.mutableListConfigCurrency.observe(requireActivity()){
+               if(it != null){
+                   //listOfShit = it
+                   recyclerAdapter.differ.submitList(it as MutableList)
+                   recycler.adapter = recyclerAdapter
+                   recycler.adapter?.notifyDataSetChanged()
+               }
+           }
+
+
+           view.findViewById<Toolbar>(R.id.topAppBar).setOnMenuItemClickListener {
+               when (it.itemId) {
+
+                   R.id.accept_setting -> {
+                       viewModel.updateCurrenciesInDb(requireContext(),
+                           viewModel.mutableListConfigCurrency.value as MutableList*//*listOfShit*//*)
                     findNavController().popBackStack()
                     true
                 }
@@ -89,6 +153,6 @@ class ConfigFragment : Fragment() {
         viewModel.getCurrency("2022-04-08", requireContext())
 
         return view
-    }
+    }*/
 
 }
--- app/src/main/java/com/example/service_currency/ui/viewmodel/ConfigViewModel.kt
+++ app/src/main/java/com/example/service_currency/ui/viewmodel/ConfigViewModel.kt
@@ -1,40 +1,67 @@
 package com.example.service_currency.ui.viewmodel
 
 import android.content.Context
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.service_currency.data.db.CurrencyEntity
 import com.example.service_currency.data.db.RoomDb
-import com.example.service_currency.data.model.Currency
 import com.example.service_currency.data.repository.CurrencyRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import androidx.compose.runtime.*
 
 class ConfigViewModel : ViewModel() {
-
-
     private val repository = CurrencyRepository()
 
-    val mutableListConfigCurrency = MutableLiveData<List<CurrencyEntity>>(null)
+    //  val mutableListConfigCurrency = MutableLiveData<List<CurrencyEntity>>(null)
+
+    var listStateCurrency: List<CurrencyEntity> by mutableStateOf(emptyList())
 
     fun getCurrency(date: String, context: Context) {
         viewModelScope.launch(Dispatchers.IO) {
             val db: RoomDb = RoomDb.getDatabase(context)
-            if(db.currencyDao().getCurrencies().isEmpty()){
-               // db.clearAllTables()
+            if (db.currencyDao().getCurrencies().isEmpty()) {
+                // db.clearAllTables()
                 val listCurrency = repository.getTodayCurrency(date)
                 val listCurrencyEntity = listCurrency.map {
-                    CurrencyEntity(it.curAbbreviation, it.curScale, it.curName, listCurrency.indexOf(it))
+                    CurrencyEntity(
+                        it.curAbbreviation,
+                        it.curScale,
+                        it.curName,
+                        listCurrency.indexOf(it)
+                    )
                 }
-                mutableListConfigCurrency.postValue(listCurrencyEntity)
+                listStateCurrency = listCurrencyEntity
                 db.currencyDao().insertFirstTimeAllCurrencies(listCurrencyEntity)
-            }
-            else{
-                val currencies = db.currencyDao().getCurrencies()
-                mutableListConfigCurrency.postValue(currencies)
+
+            } else {
+                listStateCurrency = db.currencyDao().getCurrencies()
             }
         }
+
+        /*   var list  = MutableLiveData<List<CurrencyEntity>>()
+
+           viewModelScope.launch(Dispatchers.IO){
+               val db: RoomDb = RoomDb.getDatabase(context)
+               if(db.currencyDao().getCurrencies().isEmpty()){
+                  // db.clearAllTables()
+                   val listCurrency = repository.getTodayCurrency(date)
+                   val listCurrencyEntity = listCurrency.map {
+                       CurrencyEntity(it.curAbbreviation, it.curScale, it.curName, listCurrency.indexOf(it))
+                   }
+                   mutableListConfigCurrency.postValue(listCurrencyEntity)
+                   db.currencyDao().insertFirstTimeAllCurrencies(listCurrencyEntity)
+                   list =  mutableListConfigCurrency
+               }
+               else{
+                   val currencies = db.currencyDao().getCurrencies()
+                   mutableListConfigCurrency.postValue(currencies)
+                   list = mutableListConfigCurrency
+               }
+           }
+           return list.value!!*/
     }
 
 
